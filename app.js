4c3deecd403eae71a32b437297640669559d2511
FatahRozaq <89440284+FatahRozaq@users.noreply.github.com>
2025-01-30 21:27:12 +0700

    Update app.js

--- app.js
+++ app.js
@@ -3,10 +3,11 @@ const firstName = require('./user').firstName;
 const lastName = require('./user').lastName;
 const http = require('http');
 const moment = require('moment');
+const name = "Fatah Rozaq"
 // console.log(firstName + " " + lastName + " membeli kopi " + coffee.name + " dengan harga " + coffee.price);
 // console.log(http);
 
 const date = moment().format('MMMM Do YYYY, h:mm:ss a');
-console.log(date);
+console.log(date + "ada " + name);
 
 
