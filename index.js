2543095febb51eac88957ddde099712ed284d3ea
FatahRozaq <89440284+FatahRozaq@users.noreply.github.com>
2025-01-31 21:20:49 +0700

    Update index.js

--- index.js
+++ index.js
@@ -2,4 +2,9 @@ const message = (name) => {
     return `Hello, ${name}!`;
 }
 
+const newPerson = (name) => {
+    return `New person arrived, ${name}`;
+}
+
 console.log(message("John"));
+console.log(newPerson("Doe"));
